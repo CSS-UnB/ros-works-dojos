456bf570b722fe3bb9b8be4c38dde247f53e0eeb
Perruci <pedro.perruci@gmail.com>
2018-04-26 15:43:43 -0300

    Created a position monitor to track errors between control algorithms

--- scripts/turtle_closed_loop_node.py
+++ scripts/turtle_closed_loop_node.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import rospy
 import turtle_closed_loop
+import position_monitor
 
 def print_menu():
     ''' Imprime menu e retorna opcao escolhida '''
@@ -17,16 +18,26 @@ def get_value(message):
     value = float(value)
     return value
 
+def print_position_error(monitor):
+    abs_diff, diff_x, diff_y = monitor.get_final_position_error()
+    print 'Turtle position:', monitor.get_actual_position()
+    print 'Goal position:', monitor.get_goal_position()
+    print 'X Position Error: ', diff_x
+    print 'Y Position Error: ', diff_y
+    print 'Absolute Position Error: ', abs_diff
+
 def main():
     ''' Nó executável para o controle em malha fechada '''
     rospy.init_node('turtle_closed_loop')
 
     turtle = turtle_closed_loop.TurtleClosedLoop()
-
+    monitor = position_monitor.PositionMonitor()
     while not rospy.is_shutdown():
         try:
             x, y = print_menu()
+            monitor.set_target_coordinates(x, y)
             turtle.go_to_point(x, y)
+            print_position_error(monitor)
         except rospy.ROSInterruptException:
             pass
 
